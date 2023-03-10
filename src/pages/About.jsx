fcd5764817cb649ce524a064d0128673d63c0dd2
dagbay <80764810+dagbay@users.noreply.github.com>
2023-03-10 13:30:56 +1100

    Implemented pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,14 @@
 function About() {
   return (
-    <div>About</div>
+    <>
+      <h1 className='text-6xl mb-4'>GitHub Tracker</h1>
+      <p className='mb-4 text-2xl font-light'>
+        I have developed a dynamic React application that leverages the powerful GitHub API to enable users to search for other users and obtain comprehensive user statistics. With intuitive user interface and streamlined functionality, the application delivers accurate and detailed insights on GitHub users.
+      </p>
+      <p className='text-lg text-gray-400'>
+        Version <span className='text-white'>1.0.0</span>
+      </p>
+    </>
   )
 }
 export default About
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
+import UserResults from "../components/users/UserResults"
+import UserSearch from "../components/users/UserSearch"
+
 function Home() {
   return (
-    <div>Home</div>
+    <>
+
+      <h1 className="text-5xl font-bold">Looking for a GitHub Profile?</h1>
+      <p className="py-6">With just a few clicks, you can access comprehensive user statistics and take your GitHub experience to the next level.</p>
+
+      <UserSearch />
+      <UserResults />
+    </>
   )
 }
 export default Home
