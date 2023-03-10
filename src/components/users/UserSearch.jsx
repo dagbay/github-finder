d88803acc4b9a53b1375c659695a5b1735d65201
dagbay <80764810+dagbay@users.noreply.github.com>
2023-03-10 13:31:29 +1100

    Imported all components to App.js

---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,71 @@
+import { useState, useContext } from "react"
+import GithubContext from "../../context/github/GithubContext"
+import AlertContext from "../../context/alert/AlertContext"
+
+import { searchUsers } from "../../context/github/GithubActions"
+
+import Alert from "../layout/Alert"
+
+
+function UserSearch() {
+  // Hooks
+  const { users, dispatch } = useContext(GithubContext)
+  const {setAlert} = useContext(AlertContext)
+  const [text, setText] = useState('')
+
+  // Functions
+  const handleChange = (e) => setText(e.target.value)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    if (text === '') {
+      setAlert('Please enter something', 'error')
+    } else {
+      dispatch({ type: 'SET_LOADING' })
+
+      const users = await searchUsers(text)
+
+      dispatch({ type: 'GET_USERS', payload: users})
+
+      setText('')
+    }
+  }
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+      <div>
+        <Alert />
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <div className="relative">
+              <input 
+                type="text" 
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                placeholder="Search" 
+                value={text}
+                onChange={handleChange}
+              />
+              <button 
+                type="submit" 
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
+              >
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      {users.length > 0 && (
+        <div>
+          <button onClick={() => {
+            dispatch({type: 'CLEAR_USERS'})
+          }} className="btn btn-ghost btn-lg">
+            Clear
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+export default UserSearch
