e91012c53e72b7c499eb3dceed8de2e9ec6ec20e
dagbay <80764810+dagbay@users.noreply.github.com>
2023-03-12 14:21:27 +1100

    Changed version

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ function About() {
         I have developed a dynamic React application that leverages the powerful GitHub API to enable users to search for other users and obtain comprehensive user statistics. With intuitive user interface and streamlined functionality, the application delivers accurate and detailed insights on GitHub users.
       </p>
       <p className='text-lg text-gray-400'>
-        Version <span className='text-white'>1.0.0</span>
+        Version <span className='text-white'>1.0.1</span>
       </p>
     </>
   )
